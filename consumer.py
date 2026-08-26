8383d9dc8d2921845eede6e9e501fa467ed726be
Dhruv Agarwal <dna.taurus@gmail.com>
2026-08-26 05:43:23 -0400

    consumer is pretty much done(?) onto websocket

--- consumer.py
+++ consumer.py
@@ -1,23 +1,32 @@
 from confluent_kafka import Consumer
 import json
 
-consumer_config = {
-    'bootstrap.servers': 'localhost:9092',
-    'group.id': 'coinbase_cluster'
-}
+def main():
+    consumer_config = {
+        'bootstrap.servers': 'localhost:9092',
+        'group.id': 'coinbase_cluster',
+        'auto.offset.reset': 'latest' # Start reading from the latest offset
+    }
 
-with Consumer(consumer_config) as consumer:
-    consumer.subscribe(["crypto-prices"])
+    with Consumer(consumer_config) as consumer:
+        consumer.subscribe(["crypto-prices"]) # Connect to the topic
 
-    while True:
-        raw_msg = consumer.poll(1)
-        if(raw_msg) is None:
-            print('Waiting...')
-            continue
-        else:
-            msg = json.loads(raw_message)
-            key = msg.get('symbol')
-            value = msg.get('price')
-            time = msg.get('time')
-        print (value)
-
+        while True:
+            raw_msg = consumer.poll(1)
+            if(raw_msg) is None:
+                print('Waiting...')
+                continue
+            elif raw_msg.error() is not None:
+                print('ERROR:', raw_msg.error())
+                continue
+            else:
+                msg = json.loads(raw_msg.value().decode('utf-8')) # Interpret the JSON, first decode from utf then read
+                key = msg.get('symbol')
+                value = msg.get('price')
+                time = msg.get('time')
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('\nTERMINATED CONSUMER')
+    
--- producer.py
+++ producer.py
@@ -109,4 +109,7 @@ async def main() -> None:
         producer.flush(10)
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt: # Exit producer cleanly
+            print('\nTERMINATED PRODUCER')
